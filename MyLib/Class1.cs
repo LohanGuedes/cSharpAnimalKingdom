483ca879c92cd8732739d92d333dce71ba784eda
lguedes <lohan.guedes@nutrix.tech>
2023-04-05 00:31:52 -0300

    Initial commit

---
+++ MyLib/Class1.cs
@@ -0,0 +1,62 @@
+﻿namespace MyLib;
+
+public class Animal
+{
+  public string? name { get; set; }
+  public int yearOfDiscovery { get; set; }
+  private protected static string? move;
+  private protected static string? breath;
+  private protected static string? reproduce;
+
+  public string IntroduceAnimal()
+  {
+    return ("Animal " + name + ", Year discovered: " + yearOfDiscovery);
+  }
+
+  public string MoveType()
+  {
+    return ("Move: " + move);
+  }
+
+  public string BreathType()
+  {
+    return ("Breathe: " + breath);
+  }
+
+  public string ReproduceType()
+  {
+    return ("Reproduces: " + reproduce);
+  }
+
+}
+
+public class Mamal : Animal
+{
+  static Mamal()
+  {
+    move = "Walk";
+    breath = "Lungs";
+    reproduce = "live birth";
+  }
+}
+
+public class Bird : Animal
+{
+  static Bird()
+  {
+    move = "fly";
+    breath = "Lungs";
+    reproduce = "eggs";
+  }
+
+}
+
+public class Fish : Animal
+{
+  static Fish()
+  {
+    move = "Swim";
+    breath = "gills";
+    reproduce = "eggs";
+  }
+}
---
+++ console/Program.cs
@@ -0,0 +1,59 @@
+﻿using MyLib;
+
+Mamal myMamal = new Mamal();
+
+var mamalsList = new List<Dictionary<string, object>>
+{
+    new Dictionary<string, object> { ["name"] = "Panda", ["year"] = 1869 },
+    new Dictionary<string, object> { ["name"] = "Zebra", ["year"] = 1778 },
+    new Dictionary<string, object> { ["name"] = "Koala", ["year"] = 1816 },
+    new Dictionary<string, object> { ["name"] = "Sloth", ["year"] = 1804 },
+    new Dictionary<string, object> { ["name"] = "Armadillo", ["year"] = 1758 },
+    new Dictionary<string, object> { ["name"] = "Raccoon", ["year"] = 1758 },
+    new Dictionary<string, object> { ["name"] = "BigFoot", ["year"] = 2021 }
+};
+
+var birdsList = new List<Dictionary<string, object>>
+{
+    new Dictionary<string, object> { ["name"] = "Pigeon", ["year"] = 1837 },
+    new Dictionary<string, object> { ["name"] = "Peacock", ["year"] = 1821 },
+    new Dictionary<string, object> { ["name"] = "Toucan", ["year"] = 1758 },
+    new Dictionary<string, object> { ["name"] = "Parrot", ["year"] = 1824 },
+    new Dictionary<string, object> { ["name"] = "Swan", ["year"] = 1758 }
+};
+
+var fishesList = new List<Dictionary<string, object>>
+{
+    new Dictionary<string, object> { ["name"] = "Salmon", ["year"] = 1758 },
+    new Dictionary<string, object> { ["name"] = "Catfish", ["year"] = 1817 },
+    new Dictionary<string, object> { ["name"] = "Perch", ["year"] = 1758 }
+};
+
+List<Animal> animalsList = new List<Animal>();
+
+foreach (Dictionary<string, object> mamal in mamalsList)
+{
+  Mamal obj = new Mamal();
+  obj.name = (string)mamal["name"];
+  obj.yearOfDiscovery = (int)mamal["year"];
+  animalsList.Add(obj);
+}
+
+foreach (Dictionary<string, object> bird in birdsList)
+{
+  Bird obj = new Bird();
+  obj.name = (string)bird["name"];
+  obj.yearOfDiscovery = (int)bird["year"];
+  animalsList.Add(obj);
+}
+
+foreach (Dictionary<string, object> fish in fishesList)
+{
+  Fish obj = new Fish();
+  obj.name = (string)fish["name"];
+  obj.yearOfDiscovery = (int)fish["year"];
+  animalsList.Add(obj);
+}
+
+foreach (Animal obj in animalsList)
+  Console.WriteLine(obj.IntroduceAnimal());
